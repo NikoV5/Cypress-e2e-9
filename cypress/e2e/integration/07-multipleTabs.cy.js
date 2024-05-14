9a180de6a5c619b606a781755f166c0477befdf4
NikoV5 <NikoVourtsis7@gmail.com>
2024-05-13 21:00:42 -0500

    Added updated files

--- cypress/e2e/integration/07-multipleTabs.cy.js
+++ cypress/e2e/integration/07-multipleTabs.cy.js
@@ -1,10 +1,40 @@
 describe("Handling iFrames", () => {
   beforeEach(() => {
     cy.visit("https://www.techglobal-training.com/frontend/");
-    cy.contains(".cards", "IFrames").click();
+    cy.clickCard("Multiple Windows");
+
   });
-  
-  it('iFrames', () => {
-    
-  })
+
+  it("iFrame", () => {
+    cy.get("#microsoft").should("have.attr", "target", "_blank");
+
+    cy.get("#microsoft").invoke("removeAttr", "target").click();
+
+    cy.url().should("contains", "microsoft");
+
+  });
+
+  /**
+   * Go to https://techglobal-training.com/frontend/
+   * Click on the "Multiple Windows" card
+   * Click on the "Apple" link
+   * Validate that the child window title is "Apple"
+   * Navigate back to main page
+   * Validate title contains "techglobal"
+   */
+
+  it('Test Case', () => {
+
+    cy.contains('Apple').invoke('removeAttr', 'target').click()
+
+    cy.title().should('eq', 'Apple')
+
+    cy.go(-1)
+
+    cy.title().then((el) => {
+      cy.wrap(el.toLowerCase()).should('contain', 'techglobal')
+    });
+
+  });
+
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("clickCard", (link) => {
+  cy.get(".cards").contains(link).click();
+});
