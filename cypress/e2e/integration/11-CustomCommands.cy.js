61c3989f2397bbd9fac5e627deaae2f6defc3169
NikoV5 <NikoVourtsis7@gmail.com>
2024-05-20 18:07:18 -0500

    added updated files

--- cypress/e2e/integration/11-CustomCommands.cy.js
+++ cypress/e2e/integration/11-CustomCommands.cy.js
@@ -2,9 +2,8 @@
 
 describe("Custom Commands", () => {
   beforeEach(() => {
-    cy.visit("https://www.techglobal-training.com/frontend/");
-    cy.clickCard("File Download & Upload");
-
+    cy.visit(`${Cypress.env('SITE_URL')}/frontend`);
+    cy.clickCard("Html Elements");
   });
 
 
@@ -18,7 +17,37 @@ describe("Custom Commands", () => {
     // cy.window()
     // cy.on()
 
+     cy.selectDropdownOption('#company_dropdown1', 'Apple')
+
+     cy.login('randomEmail@gmai.com', 'TechGlobal')
   });
 
-  
+  it('Child Command', () => {
+
+    // .should()
+    // .find()
+    // all the action commands
+
+    cy.get('#main_heading').then(($el) => {
+      const text = $el.text()
+
+      cy.log(text)
+    })
+
+
+    cy.get('#main_heading').logText().haveText('Html Elements')
+
+    console.log(process.env.UI_URL + 'my env')
+
+    cy.log(process.env.UI_USERNAME)
+
+    cy.log(Cypress.env('SITE_URL'))
+    cy.log(Cypress.env('UI_USERNAME'))
+    cy.log(Cypress.env('UI_PASSWORD'))
+
+    // expect(true).equal(false)
+
+
+  })
+
 });
